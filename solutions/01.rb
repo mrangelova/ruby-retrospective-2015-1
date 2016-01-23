0f4fe6ab314e2251e5d5c079fa3269b3d78081b8
Maria Rangelova <maria.rangelova93@gmail.com>
2016-01-23 21:39:21 +0200

    Update task 1 solution
    * Use better hash name
    * Use better argument names

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,9 +1,15 @@
-TO_BGN_EXCHANGE_RATE = {bgn: 1, usd: 1.7408, eur: 1.9557, gbp: 2.6415}
+TO_BGN_EXCHANGE_RATES = {
+  bgn: 1,
+  usd: 1.7408,
+  eur: 1.9557,
+  gbp: 2.6415,
+}
 
 def convert_to_bgn(price, currency)
-  (price * TO_BGN_EXCHANGE_RATE[currency]).round(2)
+  (price * TO_BGN_EXCHANGE_RATES[currency]).round(2)
 end
 
-def compare_prices(price_1, currency_1, price_2, currency_2)
-  convert_to_bgn(price_1, currency_1) <=> convert_to_bgn(price_2, currency_2)
+def compare_prices(first_price, first_currency, second_price, second_currency)
+  convert_to_bgn(first_price, first_currency)
+    <=> convert_to_bgn(second_price, second_currency)
 end
