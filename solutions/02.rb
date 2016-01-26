5282b12d63e57203e6481680d4857891596f5f39
Maria Rangelova <maria.rangelova93@gmail.com>
2016-01-27 01:05:08 +0200

    Update task 02 solution
    * Modify calculation of next head position in move_head to increase readability

--- solutions/02.rb
+++ solutions/02.rb
@@ -1,18 +1,20 @@
 def create_board(dimensions)
   xs = (0...dimensions[:width]).to_a
   ys = (0...dimensions[:height]).to_a
+
   xs.product(ys)
 end
 
 def move_head(snake, direction)
-  head = snake.last
-  head.zip(direction).map { |x, y| x + y }
+  snake_head = snake.last
+  [snake_head[0] + direction[0], snake_head[1] + direction[1]]
 end
 
 def new_food(food, snake, dimensions)
   board = create_board(dimensions)
-  free_positions = board - food - snake
-  free_positions.sample
+  free_positions_on_board = board - food - snake
+
+  free_positions_on_board.sample
 end
 
 def move(snake, direction)
@@ -26,7 +28,8 @@ end
 def obstacle_ahead?(snake, direction, dimensions)
   board = create_board(dimensions)
   next_head_position = move_head(snake, direction)
-  snake.include? next_head_position or !board.include? next_head_position
+
+  snake.include? next_head_position or not board.include? next_head_position
 end
 
 def danger?(snake, direction, dimensions)
